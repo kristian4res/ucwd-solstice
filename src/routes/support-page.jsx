4bfbfd92f161ece5542a10b7bb545e2aca470b12
Enigma-cloud <torreskristian77@gmail.com>
2022-04-21 21:53:36 +0100

    c: adding page containers to pages

--- src/routes/support-page.jsx
+++ src/routes/support-page.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import PageContainer from '../components/page-container'
 
 const ContactsPage = () => {
   return (
-    <section className='container mt-[4.7rem] bg-green-500 text-white min-h-screen min-w-full'>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum libero nesciunt vitae molestias consequuntur similique suscipit minima aut? Unde dolore eum ullam, necessitatibus perferendis facere debitis numquam laudantium culpa iusto?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis maiores culpa numquam deserunt, eveniet corrupti, assumenda, voluptatem eaque pariatur velit dicta similique tempore quisquam laborum iusto vero aliquid! Dolores, ipsa!
-    </section>
+    <PageContainer>
+      HELLO
+    </PageContainer>
   )
 }
 
