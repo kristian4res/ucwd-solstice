662ef7bec9049fca358f698374ce193495d71e9b
unknown <torreskristian77@gmail.com>
2022-04-25 12:54:51 +0100

    cf: working on trip page; added guides

--- src/components/trip-page.jsx
+++ src/components/trip-page.jsx
@@ -5,6 +5,9 @@ import AppContext from '../contexts/app-context';
 
 import PageContainer from './page-container';
 
+import { FaStar } from 'react-icons/fa';
+
+
 const TripPage = () => {
   /** CONTEXTS */
   const { devData: { trips }} = useContext(AppContext);
@@ -15,7 +18,7 @@ const TripPage = () => {
   return (
     <PageContainer>
       <div className='container flex flex-col justify-center items-center 
-        min-h-screen min-w-full px-4 pt-10 gap-4 relative
+        min-h-screen min-w-full px-4 pt-10 gap-6 relative
         sm:pt-20 md:pt-28 md:px-6
       '>
         <section className='container flex justify-center items-center 
@@ -59,13 +62,75 @@ const TripPage = () => {
             </div>
           </div>
         </section>
-        <section className='container grid grid-cols-1 justify-center items-center bg-custom-dark
+        <section className='container grid grid-cols-1 gap-4 gap-x-12
+          justify-center items-start bg-white text-black
           md:grid-cols-3
         '>
-          <div className="flex flex-col justify-center items-center w-72 h-72 col-span-2 text-white">
-            <h1>{trips[tripIndex].tripName}</h1>
+          <div className="flex flex-col justify-start items-start gap-2
+            w-full h-full col-span-2 p-6
+          ">
+            <h1 className='text-3xl font-semibold
+              md:text-4xl
+            '>
+              {trips[tripIndex].tripName}
+            </h1>
+            <h2 className='text-xl font-normal
+              md:text-2xl
+            '>
+              {trips[tripIndex].tripAddress}
+            </h2>
+            <div className='flex items-center gap-2'>
+              <FaStar className='h-4 w-4' />
+              <h2 className='flex items-end gap-2 text-xl'>
+                {trips[tripIndex].tripRating}/5
+                <span className='text-base font-light text-custom-gray-dark'>
+                  ({trips[tripIndex].tripReviews} reviews)
+                </span>
+              </h2>
+            </div>
+            <div className='flex flex-col items-center gap-2 mt-6'>
+              <p className='text-base'>
+                {trips[tripIndex].tripDescription}
+              </p>
+              <p className='text-base'>
+                {trips[tripIndex].tripDescription}
+              </p>
+            </div>
+            <div className='flex flex-col justify-start gap-2 mt-6'>
+              <h1 className='text-base font-semibold capitalize
+                  md:text-lg
+              '>
+                  Amenities
+              </h1>
+              <ul className='px-4 grid auto-cols-max justify-start items-start list-disc
+                  md:grid-cols-2 lg:w-fit lg:gap-x-12
+              '>
+                {
+                  trips[tripIndex].tripAmenities &&
+                  trips[tripIndex].tripAmenities.map((val, key) => {
+                      return (
+                          <li key={key}
+                              className='text-lg font-light'
+                          >
+                              {val}
+                          </li>
+                      )
+                  })
+                  }
+              </ul>
+            </div>
+            <div className='flex flex-col justify-start gap-2 mt-6'>
+              <h1 className='text-base font-semibold capitalize
+                  md:text-lg
+              '>
+                  Guides
+              </h1>
+              {trips[tripIndex].tripGuides[0].guideName}
+            </div>
           </div>
-          <div className="flex flex-col justify-center items-center w-72 h-72 text-white">
+          <div className="flex flex-col justify-center items-center w-full h-[600px] 
+          bg-custom-gray text-black
+          ">
             BOOKING FORM
           </div>
         </section>
